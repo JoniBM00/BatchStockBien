91579f838eb80977962969be2b36f3e140e3f4de
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-06 13:26:52 +0200

    first commit

---
+++ src/main/java/com/viewnext/BatchStockBien/BatchStockBienApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.BatchStockBien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStockBienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchStockBienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/BatchStockBien/BatchStockBienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.BatchStockBien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStockBienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
